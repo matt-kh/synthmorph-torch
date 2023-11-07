e9d1e6fedbf3ff9b9a48e11cfb6b7258bc9119e3
matthew_khoo <matthew.khoo@vitrox.com>
2023-11-07 23:31:08 +0000

    Implement blurring utilities

--- synthmorph/utils/morph.py
+++ synthmorph/utils/morph.py
@@ -409,3 +409,218 @@ def rescale_dense_transform(transform, factor, interp_method='linear'):
         rescaled = single_batch(transform)
 
     return rescaled
+
+
+def gaussian_kernel(sigma,
+                    windowsize=None,
+                    indexing='ij',
+                    separate=False,
+                    random=False,
+                    min_sigma=0,
+                    dtype=torch.float32,
+                    seed=None):
+    '''
+    Construct an N-dimensional Gaussian kernel.
+
+    Parameters:
+        sigma: Standard deviations, scalar or list of N scalars.
+        windowsize: Extent of the kernel in each dimension, scalar or list.
+        indexing: Whether the grid is constructed with 'ij' or 'xy' indexing.
+            Ignored if the kernel is separated.
+        separate: Whether the kernel is returned as N separate 1D filters.
+        random: Whether each standard deviation is uniformily sampled from the
+            interval [min_sigma, sigma).
+        min_sigma: Lower bound of the standard deviation, only considered for
+            random sampling.
+        dtype: Data type of the output. Should be floating-point.
+        seed: Integer for reproducible randomization. It is possible that this parameter only
+            has an effect if the function is wrapped in a Lambda layer.
+
+    Returns:
+        ND Gaussian kernel where N is the number of input sigmas. If separated,
+        a list of 1D kernels will be returned.
+
+    For more information see:
+        https://github.com/adalca/mivt/blob/master/src/gaussFilt.m
+
+    If you find this function useful, please consider citing:
+        M Hoffmann, B Billot, DN Greve, JE Iglesias, B Fischl, AV Dalca
+        SynthMorph: learning contrast-invariant registration without acquired images
+        IEEE Transactions on Medical Imaging (TMI), 41 (3), 543-558, 2022
+        https://doi.org/10.1109/TMI.2021.3116879
+    '''
+    # Data type.
+    assert dtype.is_floating_point, f'{dtype.name} is not a real floating-point type'
+    np_dtype = numpy_torch_dtype(dtype)
+
+    # Kernel width.
+    if not isinstance(sigma, (list, tuple)):
+        sigma = [sigma]
+    if not isinstance(min_sigma, (list, tuple)):
+        min_sigma = [min_sigma] * len(sigma)
+    sigma = [max(f, np.finfo(np_dtype).eps) for f in sigma]
+    min_sigma = [max(f, np.finfo(np_dtype).eps) for f in min_sigma]
+
+    # Kernel size.
+    if windowsize is None:
+        windowsize = [np.round(f * 3) * 2 + 1 for f in sigma]
+    if not isinstance(windowsize, (list, tuple)):
+        windowsize = [windowsize]
+    if len(sigma) != len(windowsize):
+        raise ValueError(f'sigma {sigma} and width {windowsize} differ in length')
+
+    # Precompute grid.
+    center = [(w - 1) / 2 for w in windowsize]
+    mesh = [np.arange(w) - c for w, c in zip(windowsize, center)]
+    mesh = [-0.5 * x**2 for x in mesh]
+    if not separate:
+        mesh = np.meshgrid(*mesh, indexing=indexing)
+    mesh = [torch.tensor(m, dtype=dtype, device=device) for m in mesh]     # tf.constant
+
+    # Exponents.
+    if random:
+        seeds = np.random.default_rng(seed).integers(np.iinfo(int).max, size=len(sigma))
+        max_sigma = sigma
+        sigma = []
+        for a, b, s in zip(min_sigma, max_sigma, seeds):
+            rng = torch.Generator(device=device).manual_seed(s.item())
+            sigma_val = torch.rand(size=(1,),generator=rng, dtype=dtype, device=device)
+            sigma_val = a + (b - a) * sigma_val
+            sigma.append(sigma_val)
+
+    exponent = [m / s**2 for m, s in zip(mesh, sigma)]
+
+    # Kernel.
+    if not separate:
+        exponent = [torch.sum(torch.stack(exponent), axis=0)]
+    kernel = [torch.exp(x) for x in exponent]
+    kernel = [x / torch.sum(x) for x in kernel]
+
+    return kernel if len(kernel) > 1 else kernel[0]
+
+
+def separable_conv(x,
+                   kernels,
+                   axis=None,
+                   batched=False,
+                   padding='SAME',
+                   strides=None,
+                   dilations=None):
+    '''
+    Efficiently apply 1D kernels along axes of a tensor with a trailing feature
+    dimension. The same filters will be applied across features.
+
+    Inputs:
+        x: Input tensor with trailing feature dimension.
+        kernels: A single kernel or a list of kernels, as tensors or NumPy arrays.
+            If a single kernel is passed, it will be applied along all specified axes.
+        axis: Spatial axes along which to apply the kernels, starting from zero.
+            A value of None means all spatial axes.
+        padding: Whether padding is to be used, either "VALID" or "SAME".
+        strides: Optional output stride as a scalar, list or NumPy array. If several
+            values are passed, these will be applied to the specified axes, in order.
+        dilations: Optional filter dilation rate as a scalar, list or NumPy array. If several
+            values are passed, these will be applied to the specified axes, in order.
+
+    Returns:
+        Tensor with the same type as the input.
+
+    If you find this function useful, please consider citing:
+        M Hoffmann, B Billot, DN Greve, JE Iglesias, B Fischl, AV Dalca
+        SynthMorph: learning contrast-invariant registration without acquired images
+        IEEE Transactions on Medical Imaging (TMI), 41 (3), 543-558, 2022
+        https://doi.org/10.1109/TMI.2021.3116879
+    '''
+    if isinstance(padding, str):
+        padding = padding.lower()
+
+    # Shape.
+    if not batched:
+        x = torch.unsqueeze(x, dim=0)
+    shape_space = x.shape[1:-1]
+    num_dim = len(shape_space)
+
+    # Axes.
+    if np.isscalar(axis):
+        axis = [axis]
+    axes_space = range(num_dim)
+    if axis is None:
+        axis = axes_space
+    assert all(ax in axes_space for ax in axis), 'non-spatial axis passed'
+
+    # Conform strides and dilations.
+    ones = np.ones(num_dim, np.int32)
+    f = map(lambda x: 1 if x is None else x, (strides, dilations))
+    f = map(np.ravel, f)
+    f = map(np.ndarray.tolist, f)
+    f = map(lambda x: x * len(axis) if len(x) == 1 else x, f)
+    f = map(lambda x: [(*ones[:ax], x[i], *ones[ax + 1:]) for i, ax in enumerate(axis)], f)
+    strides, dilations = f
+    assert len(strides) == len(axis), 'number of strides and axes differ'
+    assert len(dilations) == len(axis), 'number of dilations and axes differ'
+
+    # Conform kernels.
+    if not isinstance(kernels, (tuple, list)):
+        kernels = [kernels]
+    if len(kernels) == 1:
+        kernels = kernels.copy() * len(axis)
+    assert len(kernels) == len(axis), 'number of kernels and axes differ'
+
+    # Merge features and batches.
+    ind = np.arange(num_dim + 2)
+    forward = (0, ind[-1], *ind[1:-1])
+    backward = (0, *ind[2:], 1)
+    x = torch.permute(x, forward)
+    shape_bc = x.shape[:2]
+    merge_shape = np.concatenate(
+        (
+            np.prod(shape_bc, keepdims=True),
+            shape_space,
+            [1],
+        ),
+        axis=0,
+    )
+    x = torch.reshape(x, shape=tuple(merge_shape))
+
+    # Convolve.
+    for ax, k, s, d in zip(axis, kernels, strides, dilations):
+        width = np.prod(k.shape)
+        k = torch.reshape(k, shape=(*ones[:ax], width, *ones[ax + 1:], 1, 1))
+        x = nnf.conv2d(x, k, padding=padding, stride=s, dilation=d)     # tf.nn.convolution
+
+    # Restore dimensions.
+    restore_shape = np.concatenate((shape_bc, tuple(x.shape)[1:-1]), axis=0)
+    x = torch.reshape(x, shape=tuple(restore_shape))
+    x = torch.permute(x, backward)
+
+    return x if batched else x[0, ...]
+
+
+def numpy_torch_dtype(dtype):
+    """
+    Source:
+        https://github.com/pytorch/pytorch/blob/v2.1.0/torch/testing/_internal/common_utils.py#L1057
+        Lines 1221 - 1234
+    """
+    numpy_to_torch_dtype_dict = {
+        np.bool_      : torch.bool,
+        np.uint8      : torch.uint8,
+        np.int8       : torch.int8,
+        np.int16      : torch.int16,
+        np.int32      : torch.int32,
+        np.int64      : torch.int64,
+        np.float16    : torch.float16,
+        np.float32    : torch.float32,
+        np.float64    : torch.float64,
+        np.complex64  : torch.complex64,
+        np.complex128 : torch.complex128
+    }
+
+    # Dict of torch dtype -> NumPy dtype
+    torch_to_numpy_dtype_dict = {value : key for (key, value) in numpy_to_torch_dtype_dict.items()}
+    torch2numpy = lambda x: torch_to_numpy_dtype_dict[x]
+    
+    result = numpy_to_torch_dtype_dict.get(dtype, torch2numpy(dtype))
+    assert result is not None, "Dtype not found"
+    
+    return result
