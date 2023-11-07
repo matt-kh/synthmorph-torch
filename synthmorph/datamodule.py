4d63a11c3c239610b5fb88af2d65018b2dce170d
matthew_khoo <matthew.khoo@vitrox.com>
2023-11-07 23:39:41 +0000

    Implement label to image and refactor to Torch

--- synthmorph/datamodule.py
+++ synthmorph/datamodule.py
@@ -1,15 +1,15 @@
 import warnings
 from typing import List
 import torch
-import cv2
+import torch.nn.functional as nnf
 import numpy as np
-from scipy.interpolate import interpn
-from scipy.ndimage.filters import gaussian_filter
 from torch.utils.data import Dataset
+from torchvision.transforms import GaussianBlur
 from tqdm import tqdm
 
 # local code
 from . import utils
+from . import layers
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -64,76 +64,17 @@ def draw_perlin(out_shape,
         zoom = [o / s for o, s in zip(out_shape, sample_shape)]
         out += gauss if scale == 1 else utils.resize(gauss, zoom[:-1])
 
+    # Transform to Torch format
+    indices = list(range(len(out.shape)))
+    out = out.permute(-1, *indices[:-1])
+
     return out
 
 
-def compute_points(src: List[int], target: List[int] = None):
-    if target is None:
-        target = src
-
-    assert len(src) == len(target), "len(src) != len(target)"
-    lins = []
-    for s, t in zip(src, target):
-        l = np.linspace(0., s - 1., t)
-        lins.append(l)
-
-    return lins
 
 
-def preprocess_interpn(vol: np.ndarray):
-    for i, s in enumerate(vol.shape):
-        if s == 1:
-            vol = np.concatenate([vol, vol], axis=i)
-
-    return vol
-
-def resize_np(vol: np.ndarray,
-              target_shapes: List[int],
-              interp_method='linear'):
-    assert len(target_shapes) == len(vol.shape), "target_shapes must match len(vol.shape)"
-    vol = preprocess_interpn(vol)
-
-    grids = compute_points(vol.shape, target_shapes)
-    out_grids = np.meshgrid(*grids, indexing='ij')
-    out_grids = np.stack(out_grids, -1)
-
-    points = compute_points(vol.shape)
-    inter = interpn(points, vol, out_grids, method=interp_method)
-
-    return inter
 
 
-def transform(vol: np.ndarray,
-              loc_shift: np.ndarray,
-              interp_method='linear',
-              fill_value=None):
-    original_shape = vol.shape
-
-    vol = preprocess_interpn(vol)
-    loc_shift = preprocess_interpn(loc_shift)
-
-    points = compute_points(vol.shape)
-    out_grids = np.meshgrid(*points, indexing='ij')
-    out_grids = np.stack(out_grids, -1)
-
-    cross_channel = np.zeros([*out_grids.shape[:-1], 1])
-    loc_shift = np.concatenate([loc_shift, cross_channel], axis=len(out_grids.shape) -1)
-
-    # breakpoint()
-    out_grids = out_grids + loc_shift
-
-    inter = interpn(points, vol, out_grids, method=interp_method,
-                    # there is a possibility for out_grid to be out of bound.
-                    # at this point, we need to extrapolate the value.
-                    bounds_error=False, fill_value=fill_value)
-
-    index = [slice(None)] * len(original_shape) 
-    for i, s in enumerate(original_shape):
-        if s == 1:
-            index[i] = 0
-
-    inter = inter[tuple(index)]
-    return inter
 
 
 def generate_map(size: List[int] = [256, 256],
@@ -148,80 +89,17 @@ def generate_map(size: List[int] = [256, 256],
     num_dim = len(size)
     out = draw_perlin([*size, nLabel], [32, 64], max_std=1, seed=seed1, device=device)
     warp = draw_perlin([*size, nLabel, num_dim], [16, 32, 64], max_std=16, seed=seed2, device=device)
-
+    
+    out_indices = np.arange(len(out.shape))
+    warp_indices = np.arange(len(warp.shape))
+    out = out.permute(*out_indices[1:], 0)
+    warp = warp.permute(*warp_indices[1: -1], -1, 0)
     deform = utils.transform(out, warp)
 
     map = torch.argmax(deform, dim=-1)
     return map.to(torch.uint8)
 
 
-def vec_intergral(vec: np.ndarray, nsteps=5):
-    scale = 1.0 / 2 ** nsteps
-    vec = vec * scale
-    for _ in range(nsteps):
-        vec_field = np.expand_dims(vec, -1)
-        vec_field = np.repeat(vec_field, vec.shape[-1], axis=-1)
-        vec = vec + transform(vec, vec_field)
-
-    return vec
-
-
-def map_to_image(label_map: np.ndarray):
-    rand_gen = np.random.default_rng()
-
-    num_dim = len(label_map.shape)
-    shape = np.array(label_map.shape)
-   
-    warp_shape = [*(shape // 2), num_dim]
-    warp_res =  np.int32(np.array([8, 16, 32]) / 2)
-    warp_field = draw_perlin(warp_shape, warp_res, 
-                             min_std=3, max_std=2)
-
-    warp_field = vec_intergral(warp_field, nsteps=5)
-    warp_field = warp_field * 2
-    warp_field = cv2.resize(warp_field, label_map.shape)
-
-    label_map = np.expand_dims(label_map, 2)
-    warp_field = np.expand_dims(warp_field, 2)
-    label_map = transform(label_map, warp_field, interp_method='nearest')
-
-    label_map = label_map.astype(np.int32)
-    out = random_label(label_map)
-
-    # randomly zero out some background
-    rand_flip = rand_gen.uniform(low=0, high=1, size=out.shape)
-    rand_flip = rand_flip < 0.2 ** 16
-    out *= 1 - rand_flip
-    
-    out = gaussian_filter(out, 1)   # gaussian blur
-
-    bias_field = draw_perlin([*shape, 1],
-                             scales=[40],
-                             min_std=0.3, max_std=0.3)
-    bias_field = np.squeeze(bias_field, -1)
-    out *= np.exp(bias_field)
-
-    # Intensity manipulation
-    out = np.clip(out, 0, 255)
-    # out = minmax_norm(out)
-    gamma = rand_gen.normal(loc=0.0, scale=0.25, size=out.shape)
-    out = np.power(out, np.exp(gamma))
-    out = np.expand_dims(out, -1)
-
-    return label_map, out
-
-
-def random_label(label_map, low_mean=25, max_mean=255, low_std=5, max_std=25,
-                random=np.random.RandomState(None)):
-    out = random.uniform(size=label_map.shape)
-
-    for j in np.unique(label_map):
-        mean = random.uniform(low_mean, max_mean)
-        std = random.uniform(low_std, max_std)
-
-        out[label_map == j] = out[label_map == j] * std + mean
-
-    return out
 
 
 def minmax_norm(x, axis=None):
@@ -312,3 +190,261 @@ class SynthMorphDataset(Dataset):
         }
         
         return results
+    
+    
+def labels_to_image(
+    labels,
+    out_label_list=None,
+    out_shape=None,
+    num_chan=1,
+    mean_min=None,
+    mean_max=None,
+    std_min=None,
+    std_max=None,
+    zero_background=0.2,
+    warp_res=[16],
+    warp_std=0.5,
+    warp_modulate=True,
+    bias_res=40,
+    bias_std=0.3,
+    bias_modulate=True,
+    blur_std=1,
+    blur_modulate=True,
+    normalize=True,
+    gamma_std=0.25,
+    dc_offset=0,
+    one_hot=True,
+    seeds={},
+    return_vel=False,
+    return_def=False,
+    id=0,
+    device=device
+):
+    """
+    Generative model for augmenting label maps and synthesizing images from them.
+
+    Parameters:
+        out_label_list (optional): List of labels in the output label maps. If
+            a dictionary is passed, it will be used to convert labels, e.g. to
+            GM, WM and CSF. All labels not included will be converted to
+            background with value 0. If 0 is among the output labels, it will be
+            one-hot encoded. Defaults to the input labels.
+        out_shape (optional): List of the spatial dimensions of the outputs.
+            Inputs will be symmetrically cropped or zero-padded to fit.
+            Defaults to the input shape.
+        num_chan (optional): Number of image channels to be synthesized.
+            Defaults to 1.
+        mean_min (optional): List of lower bounds on the means drawn to generate
+            the intensities for each label. Defaults to 0 for the background and
+            25 for all other labels.
+        mean_max (optional): List of upper bounds on the means drawn to generate
+            the intensities for each label. Defaults to 225 for each label.
+        std_min (optional): List of lower bounds on the SDs drawn to generate
+            the intensities for each label. Defaults to 0 for the background and
+            5 for all other labels.
+        std_max (optional): List of upper bounds on the SDs drawn to generate
+            the intensities for each label. Defaults to 25 for each label.
+            25 for all other labels.
+        zero_background (float, optional): Probability that the background is set
+            to zero. Defaults to 0.2.
+        warp_res (optional): List of factors N determining the
+            resultion 1/N relative to the inputs at which the SVF is drawn.
+            Defaults to 16.
+        warp_std (float, optional): Upper bound on the SDs used when drawing
+            the SVF. Defaults to 0.5.
+        warp_modulate (bool, optional): Whether to draw the SVF with random SDs.
+            If disabled, each batch will use the maximum SD. Defaults to True.
+        bias_res (optional): List of factors N determining the
+            resultion 1/N relative to the inputs at which the bias field is
+            drawn. Defaults to 40.
+        bias_std (float, optional): Upper bound on the SDs used when drawing
+            the bias field. Defaults to 0.3.
+        bias_modulate (bool, optional): Whether to draw the bias field with
+            random SDs. If disabled, each batch will use the maximum SD.
+            Defaults to True.
+        blur_std (float, optional): Upper bound on the SD of the kernel used
+            for Gaussian image blurring. Defaults to 1.
+        blur_modulate (bool, optional): Whether to draw random blurring SDs.
+            If disabled, each batch will use the maximum SD. Defaults to True.
+        normalize (bool, optional): Whether the image is min-max normalized.
+            Defaults to True.
+        gamma_std (float, optional): SD of random global intensity
+            exponentiation, i.e. gamma augmentation. Defaults to 0.25.
+        dc_offset (float, optional): Upper bound on global DC offset drawn and
+            added to the image after normalization. Defaults to 0.
+        one_hot (bool, optional): Whether output label maps are one-hot encoded.
+            Only the specified output labels will be included. Defaults to True.
+        seeds (dictionary, optional): Integers for reproducible randomization.
+        return_vel (bool, optional): Whether to append the half-resolution SVF
+            to the model outputs. Defaults to False.
+        return_def (bool, optional): Whether to append the combined displacement
+            field to the model outputs. Defaults to False.
+        id (int, optional): Model identifier used to create unique layer names
+            for including this model multiple times. Defaults to 0.
+    """
+    np_rng = np.random.default_rng(None)
+    default_seed = lambda: np_rng.integers(np.iinfo(int).max).item()
+    rng = lambda x : torch.Generator(device=device).manual_seed(x)
+
+
+    in_shape = labels.shape
+    if out_shape is None:
+        out_shape = in_shape
+    in_shape, out_shape = map(np.asarray, (in_shape, out_shape))
+    num_dim = len(in_shape)
+
+    # Torch to TF tensor format
+    labels = labels.view(1, 1, *in_shape)
+    
+    # Inputs
+    is_not_integer = lambda x: torch.is_floating_point(x) or torch.is_complex(x)
+    if is_not_integer(labels):
+        labels = labels.to(torch.int32)
+    batch_size = labels.shape[0]
+    
+    # Transform labels into [0, 1, ..., N-1].
+    if labels.is_cuda:
+        in_label_list = labels.unique().detach().cpu().numpy()
+    else:
+        in_label_list = labels.unique().numpy()
+    num_in_labels = len(in_label_list)
+
+    # in_lut = torch.zeros(torch.max(in_label_list) + 1, dtype=torch.float32)
+    # for i, lab in enumerate(in_label_list):
+    #     in_lut[lab] = i
+    # labels = in_lut[labels] # tf.gather(in_lut, indices=labels) 
+
+    if warp_std > 0:
+        # Velocity field.
+        vel_shape = (*out_shape // 2, num_dim)
+        vel_scale = np.asarray(warp_res) / 2
+        vel_draw = lambda x: draw_perlin(
+            vel_shape, scales=vel_scale,
+            min_std=0 if warp_modulate else warp_std, max_std=warp_std,
+            seed=seeds.get('warp')
+        )
+        # One per batch.
+        vel_field = torch.stack([vel_draw(l) for l in labels])
+        # Deformation field.
+        def_field = layers.VecInt(int_steps=5)(vel_field)
+        def_field = layers.RescaleValues(2)(def_field)
+        def_field = layers.Resize(2, interp_method='linear')(def_field)
+        # Resampling.
+        labels = layers.SpatialTransformer(interp_method='nearest', fill_value=0)([labels, def_field])
+
+    labels = labels.to(torch.int32)
+    # Intensity means and standard deviations.
+    if mean_min is None:
+        mean_min = [0] + [25] * (num_in_labels - 1)
+    if mean_max is None:
+        mean_max = [225] * num_in_labels
+    if std_min is None:
+        std_min = [0] + [5] * (num_in_labels - 1)
+    if std_max is None:
+        std_max = [25] * num_in_labels
+    as_torch_tensor = lambda x: torch.as_tensor(x, device=device)
+    m0, m1, s0, s1 = map(as_torch_tensor, (mean_min, mean_max, std_min, std_max))
+
+    mean = torch.rand(
+        size=(batch_size, num_chan, num_in_labels),
+        generator=rng(seeds.get('mean', default_seed())),
+        device=device,
+    )
+    mean = m0 + (m1 - m0) * mean
+
+    std = torch.rand(
+        size=(batch_size, num_chan, num_in_labels),
+        generator=rng(seeds.get('std', default_seed())),
+        device=device,
+    )
+    std = s0 + (s1 - s0) * std
+
+    # Synthetic image.
+    image = torch.empty(size=labels.shape, device=device).normal_(generator=rng(seeds.get('noise', default_seed())))
+    indices = torch.concat([labels + i * num_in_labels for i in range(num_chan)], dim=-1)
+    gather = lambda x: torch.reshape(x[0], (-1,))[x[1]]
+    mean = gather([mean, indices])
+    std = gather([std, indices])
+    image = image * std + mean
+
+    # Zero background.
+    if zero_background > 0:
+        rand_flip = torch.rand(
+            size=(batch_size, *[1] * num_dim, num_chan),
+            generator=rng(seeds.get('background', default_seed())),
+            device=device,
+        )
+        rand_flip = torch.lt(rand_flip, zero_background)    # tf.less
+        image *= 1. - torch.logical_and(labels == 0, rand_flip).to(image.dtype)
+
+    # Blur.
+    if blur_std > 0:
+        kernels = utils.gaussian_kernel(
+            [blur_std] * num_dim, separate=True, random=blur_modulate,
+            dtype=image.dtype, seed=seeds.get('blur'),
+        )
+        image = utils.separable_conv(image, kernels, batched=True)
+
+    # Bias field.
+    if bias_std > 0:
+        bias_shape = (*out_shape, 1)
+        bias_draw = lambda x: draw_perlin(
+            bias_shape, scales=bias_res, seed=seeds.get('bias'),
+            min_std=0 if bias_modulate else bias_std, 
+            max_std=bias_std, device=device
+        )
+        bias_field = torch.stack([bias_draw(l) for l in labels])
+        image *= torch.exp(bias_field)
+
+    # Intensity manipulations.
+    image = torch.clip(image, min=0, max=255)
+    if normalize:
+        image = torch.stack([minmax_norm(batch) for batch in image])  
+    if gamma_std > 0:
+        gamma = torch.empty(size=(batch_size, *[1] * num_dim, num_chan), device=device)
+        gamma = gamma.normal_(std=gamma_std, generator=rng(seeds.get('gamma', default_seed())))
+        image = torch.pow(image, torch.exp(gamma))
+    if dc_offset > 0:
+        offset = torch.rand(
+            size=(batch_size, *[1] * num_dim, num_chan),
+            generator=(rng(seeds.get('dc_offset'), default_seed())),
+            device=device
+        )
+        offset *= dc_offset
+        image += offset
+
+    # Lookup table for converting the index labels back to the original values,
+    # setting unwanted labels to background. If the output labels are provided
+    # as a dictionary, it can be used e.g. to convert labels to GM, WM, CSF.
+    if out_label_list is None:
+        out_label_list = in_label_list
+    if isinstance(out_label_list, (tuple, list, np.ndarray)):
+        out_label_list = {lab: lab for lab in out_label_list}
+    out_lut = np.zeros(num_in_labels, dtype='int32')
+    for i, lab in enumerate(in_label_list):
+        if lab in out_label_list:
+            out_lut[i] = out_label_list[lab]
+
+    # For one-hot encoding, update the lookup table such that the M desired
+    # output labels are rebased into the interval [0, M-1[. If the background
+    # with value 0 is not part of the output labels, set it to -1 to remove it
+    # from the one-hot maps.
+    if one_hot:
+        hot_label_list = np.unique(list(out_label_list.values()))  # Sorted.
+        hot_lut = np.full(hot_label_list[-1] + 1, fill_value=-1, dtype='int32')
+        for i, lab in enumerate(hot_label_list):
+            hot_lut[lab] = i
+        out_lut = torch.tensor(hot_lut[out_lut], device=device)
+
+    # Convert indices to output labels only once.
+    labels = out_lut[labels]
+    if one_hot:
+        labels = nnf.one_hot(labels.to(torch.int64)[..., 0], num_classes=len(hot_label_list))
+
+    outputs = {'image': image, 'label': labels}
+    if return_vel:
+        outputs['vel'] = vel_field
+    if return_def:
+        outputs['def'] =  def_field
+
+    return outputs
