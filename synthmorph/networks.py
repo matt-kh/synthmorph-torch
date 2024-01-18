39bd1eedce089854166ddf51258979a840db647a
matthew-khoo <matthew.khoo@vitrox.com>
2024-01-18 09:10:36 +0800

    WIP: Implement custom Unet and affine detector

--- synthmorph/networks.py
+++ synthmorph/networks.py
@@ -96,6 +96,7 @@ class Unet(nn.Module):
                  half_res=False):
         """
         Parameters:
+            conv_block: The set of layers used for a single Unet block
             inshape: Input shape. e.g. (192, 192, 192)
             infeats: Number of input features.
             nb_features: Unet convolutional features. Can be specified via a list of lists with
@@ -109,7 +110,6 @@ class Unet(nn.Module):
             nb_conv_per_level: Number of convolutions per unet level. Default is 1.
             half_res: Skip the last decoder upsampling. Default is False.
         """
-
         super().__init__()
 
         # ensure correct dimensionality
@@ -165,6 +165,7 @@ class Unet(nn.Module):
                 prev_nf = nf
             self.encoder.append(convs)
             encoder_nfs.append(prev_nf)
+        
         # configure decoder (up-sampling path)
         encoder_nfs = np.flip(encoder_nfs)
         self.decoder = nn.ModuleList()
@@ -181,7 +182,7 @@ class Unet(nn.Module):
 
         # now we take care of any remaining convolutions
         self.remaining = nn.ModuleList()
-        for num, nf in enumerate(final_convs):
+        for _, nf in enumerate(final_convs):
             self.remaining.append(ConvBlock(ndims, prev_nf, nf))
             prev_nf = nf
 
@@ -210,7 +211,150 @@ class Unet(nn.Module):
             x = conv(x)
 
         return x
-    
+
+
+class UnetAffine(nn.Module):
+    """
+    A unet architecture. Layer features can be specified directly as a list of encoder and decoder
+    features or as a single integer along with a number of unet levels. The default network features
+    per layer (when no options are specified) are:
+        encoder: [16, 32, 32, 32]
+        decoder: [32, 32, 32, 32, 32, 16, 16]
+    """
+
+    def __init__(self,
+                 inshape=None,
+                 infeats=None,
+                 nb_features=None,
+                 rem_features = None,
+                 nb_levels=None,
+                 max_pool=2,    
+                 feat_mult=1,
+                 nb_conv_per_level=1,
+                 half_res=False):
+        """
+        Parameters:
+            conv_block: The set of layers used for a single Unet block
+            inshape: Input shape. e.g. (192, 192, 192)
+            infeats: Number of input features.
+            nb_features: Unet convolutional features. Can be specified via a list of lists with
+                the form [[encoder feats], [decoder feats]], or as a single integer. 
+                If None (default), the unet features are defined by the default config described in 
+                the class documentation.
+            nb_levels: Number of levels in unet. Only used when nb_features is an integer. 
+                Default is None.
+            feat_mult: Per-level feature multiplier. Only used when nb_features is an integer. 
+                Default is 1.
+            nb_conv_per_level: Number of convolutions per unet level. Default is 1.
+            half_res: Skip the last decoder upsampling. Default is False.
+        """
+        super().__init__()
+
+        # ensure correct dimensionality
+        ndims = len(inshape)
+        assert ndims in [1, 2, 3], 'ndims should be one of 1, 2, or 3. found: %d' % ndims
+
+        # cache some parameters
+        self.half_res = half_res
+
+        # default encoder and decoder layer features if nothing provided
+        if nb_features is None:
+            nb_features = [
+                [16, 32, 32, 32],             # encoder
+                [32, 32, 32, 32, 32, 16, 16]  # decoder
+            ]
+
+        # build feature list automatically
+        if isinstance(nb_features, int):
+            if nb_levels is None:
+                raise ValueError('must provide unet nb_levels if nb_features is an integer')
+            feats = np.round(nb_features * feat_mult ** np.arange(nb_levels)).astype(int)
+            nb_features = [
+                np.repeat(feats[:-1], nb_conv_per_level),
+                np.repeat(np.flip(feats), nb_conv_per_level)
+            ]
+        elif nb_levels is not None:
+            raise ValueError('cannot use nb_levels if nb_features is not an integer')
+
+        # extract any surplus (full resolution) decoder convolutions
+        enc_nf, dec_nf = nb_features
+        nb_dec_convs = len(enc_nf)
+        if len(dec_nf) < nb_dec_convs:
+            nb_dec_convs = len(dec_nf)
+            final_convs = dec_nf + rem_features
+            dec_nf = []
+        else:
+            final_convs = dec_nf[nb_dec_convs:]
+            dec_nf = dec_nf[:nb_dec_convs]
+
+        self.nb_levels = int(nb_dec_convs / nb_conv_per_level) + 1
+
+        if isinstance(max_pool, int):
+            max_pool = [max_pool] * self.nb_levels
+
+        # cache downsampling / upsampling operations
+        MaxPooling = getattr(nn, 'MaxPool%dd' % ndims)
+        self.pooling = [MaxPooling(s) for s in max_pool]
+        self.upsampling = [nn.Upsample(scale_factor=s, mode='nearest') for s in max_pool]
+
+        # configure encoder (down-sampling path)
+        prev_nf = infeats
+        encoder_nfs = [prev_nf]
+        self.encoder = nn.ModuleList()
+        for level in range(self.nb_levels - 1):
+            convs = nn.ModuleList()
+            for conv in range(nb_conv_per_level):
+                nf = enc_nf[level * nb_conv_per_level + conv]
+                convs.append(ConvDropBlock(ndims, prev_nf, nf))
+                prev_nf = nf
+            self.encoder.append(convs)
+            encoder_nfs.append(prev_nf)
+        
+        # configure decoder (up-sampling path)
+        encoder_nfs = np.flip(encoder_nfs)
+        self.decoder = nn.ModuleList()
+        for level in range(self.nb_levels - 1):
+            convs = nn.ModuleList()
+            for conv in range(nb_conv_per_level):
+                nf = dec_nf[level * nb_conv_per_level + conv]
+                
+                convs.append(ConvDropBlock(ndims, prev_nf, nf))
+                prev_nf = nf
+            self.decoder.append(convs)
+            if not half_res or level < (self.nb_levels - 2):
+                prev_nf += encoder_nfs[level]
+
+        # now we take care of any remaining convolutions
+        self.remaining = nn.ModuleList()
+        for _, nf in enumerate(final_convs):
+            self.remaining.append(ConvDropBlock(ndims, prev_nf, nf))
+            prev_nf = nf
+
+        # cache final number of features
+        self.final_nf = prev_nf
+
+    def forward(self, x):
+        # encoder forward pass
+        x_history = [x]
+        for level, convs in enumerate(self.encoder):
+            for conv in convs:
+                x = conv(x)
+            x_history.append(x)
+            x = self.pooling[level](x)
+
+        # decoder forward pass with upsampling and concatenation
+        for level, convs in enumerate(self.decoder):
+            for conv in convs:
+                x = conv(x)
+            if not self.half_res or level < (self.nb_levels - 2):
+                x = self.upsampling[level](x)
+                x = torch.cat([x, x_history.pop()], dim=1)
+
+        # remaining convs at full resolution
+        for conv in self.remaining:
+            x = conv(x)
+
+        return x
 
 class VxmDense(nn.Module):
     """
@@ -447,7 +591,6 @@ class VxmAffineFeatureDetector(nn.Module):
         
         dtype = torch.get_default_dtype()
 
-        # Detector inputs
         self.halfres_st = layers.SpatialTransformer(fill_value=0, shape=shape_half, shift_center=False)
 
         # Downsampling and upsampling operations
@@ -500,6 +643,9 @@ class VxmAffineFeatureDetector(nn.Module):
                 )
             )
             prev_nf = nf
+        
+        self.det = nn.ModuleList(self.encoder + self.decoder + self.remaining)
+
 
         # Output features
         out_args = dict(kernel_size=3, padding='same')
@@ -507,10 +653,47 @@ class VxmAffineFeatureDetector(nn.Module):
         self.out.append(conv(prev_nf, num_feat, **out_args))
         self.out.append(nn.ReLU())
 
-        barycenter_args = dict(axes=range(1, ndims + 1), normalize=True, shift_center=True, dtype=dtype)
-        self.barycenter = lambda x: utils.barycenter(x, **barycenter_args) * shape_full
+        com_args = dict(axes=range(1, ndims + 1), normalize=True, shift_center=True, dtype=dtype)
+        self.com = layers.CenterOfMass(**com_args)
+        self.ls_fit = layers.LeastSquaresFit()
+        self.params_to_aff = layers.ParamsToAffineMatrix(ndims=ndims)
+        self.compose = layers.ComposeTransform(shift_center=False)
+        self.aff_to_dense = layers.AffineToDenseShift(shift_center=False)
+        
+        shape_out = shape_half if return_trans_to_half_res else shape_full
+        self.out_st = layers.SpatialTransformer(shift_center=False, fill_value=0, shape=shape_out)
         
 
-    def forward(self):
-        return
+    def forward(self, source, target):
+        """
+        Parameters:
+            source: Source image tensor.
+            target: Target image tensor.
+            registration: Return integrated flow during inference. Default is False.
+        """
+        x = torch.cat([source, target], dim=1)
+        x = self.det(x)
         
+
+        return
+    
+    # Static transforms. Function names refer to effect on coordinates.
+    def _tensor(self, x, shape, dtype=torch.float32):
+        x = torch.tensor(x[None, :-1, :], dtype=dtype)
+        return x.repeat(shape[0], 1, 1)
+
+    def _cen(self, ndims, shape):
+        mat = torch.eye(ndims + 1)
+        mat[:-1, -1] = -0.5 * (shape - 1)
+        return self._tensor(mat)
+    
+    def _un_cen(self, ndims, shape):
+        mat = torch.eye(ndims + 1)
+        mat[:-1, -1] = +0.5 * (shape - 1)
+        return self._tensor(mat)
+
+    def _scale(self, ndims, fact):
+        mat = torch.diag(torch.tensor([fact] * ndims + [1.0]))
+        return self._tensor(mat)
+
+    
